d03ab7311043b067a676540cefdce2225028d0f6
Tom Klaver <tomklav@gmail.com>
2019-04-10 15:42:38 +0200

    SSR graphql WIP

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  useFileSystemPublicRoutes: false,
   serverRuntimeConfig: {
     // Will only be available on the server side
     mySecret: 'secret',
--- proxy.js
+++ proxy.js
@@ -7,7 +7,7 @@ const corsProxy = require('cors-anywhere');
 
 corsProxy.createServer({
   originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
+  // requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
