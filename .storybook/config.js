e1727d4e43dcffcf093a8b93e216ec3724ea0ee7
federodes <federodes@gmail.com>
2019-05-08 13:25:12 +0200

    add all stories.js to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../theme';
 
-const req = require.context('../stories', true, /\.stories\.js$/);
-// const req = require.context('../layouts', true, /\.stories\.js$/);
+const req = require.context('../', true, /stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
