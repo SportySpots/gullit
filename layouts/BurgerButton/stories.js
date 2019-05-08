1235088da71ba311b49e84633f74330e1cbc51f8
federodes <federodes@gmail.com>
2019-05-08 14:02:33 +0200

    introducing header and BurgerButton comps

---
+++ layouts/BurgerButton/stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import BurgerButton from '.';
+
+storiesOf('layouts.BurgerButton', module)
+  .add('BurgerButton', () => <BurgerButton />);
