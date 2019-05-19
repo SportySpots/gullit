45e731f32942dc111cad436855a9e99a93d45a21
federodes <federodes@gmail.com>
2019-05-19 22:04:56 +0200

    Hide header burger btn

--- layouts/Header/index.js
+++ layouts/Header/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Flex, Box } from 'rebass';
 import styled from 'styled-components';
-import BurgerButton from '../BurgerButton';
+// import BurgerButton from '../BurgerButton';
 
+//------------------------------------------------------------------------------
+// STYLE:
+//------------------------------------------------------------------------------
 const Container = styled(Flex)`
-  /* height: 80px; */
+  height: 60px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.shade}
 `;
-
+//------------------------------------------------------------------------------
+// COMPONENT:
+//------------------------------------------------------------------------------
 const Header = () => (
   <Container justifyContent="space-between">
     <Box>
       Logo
     </Box>
-    <Box>
+    {/* <Box>
       <BurgerButton />
-    </Box>
+    </Box> */}
   </Container>
 );
 
